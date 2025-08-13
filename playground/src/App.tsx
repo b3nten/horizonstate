050a61abab5b01aab2f47132e4d128de66893fe0
benton <ben@benten.ca>
2025-08-13 16:27:03 -0700

    feat: clean up react exports

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { Model } from "../../src/mod";
+import { useModel } from "../../src/react";
 
-function App() {
-  const [count, setCount] = useState(0)
+let sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+let model = new Model({
+  a: 1,
+  b: {
+    c: 3,
+  },
+});
+
+let increment_a = model.addTransaction(
+  async (_: void, { update, optimisticUpdate }) => {
+    optimisticUpdate((model) => {
+      model.a = model.a + 1;
+    });
+    await sleep(1000);
+    update((model) => {
+      model.a = model.a + 1;
+    });
+  },
+);
+
+let increment_b = model.addTransaction(
+  async (_: void, { applyOptimisticUpdates, optimisticUpdate }) => {
+    optimisticUpdate((model) => {
+      model.b.c = model.b.c + 1;
+    });
+    await sleep(1000);
+    applyOptimisticUpdates();
+  },
+);
+
+export let AppRoot = () => {
+  let state = useModel(model);
+
+  let b = useModel(model, (m) => m.b);
+
+  useEffect(() => {
+    console.log("Model updated");
+  }, [state]);
+
+  useEffect(() => {
+    console.log("Model.b updated");
+  }, [b]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <div className="w-lg mx-auto p-4">
+      <button className="btn btn-primary" onClick={() => increment_a.run()}>
+        Increment A: {state.a}
+      </button>
+      <button className="btn btn-primary" onClick={() => increment_b.run()}>
+        Increment B: {b.c}
+      </button>
+    </div>
+  );
+};
